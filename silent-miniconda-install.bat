06f881fc99dd887ce66a647b67bb87369c47a976
Jason Pruitt <pruitt.jason8164@gmail.com>
2025-08-21 11:38:34 -0400

    Add script for silent windows miniconda install

---
+++ silent-miniconda-install.bat
@@ -0,0 +1,3 @@
+curl https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe --output .\Downloads\Miniconda3-latest-Windows-x86_64.exe
+
+start /wait "" Miniconda3-latest-Windows-x86_64.exe /InstallationType=JustMe /RegisterPython=0 /S /D=%UserProfile%\Miniconda3
